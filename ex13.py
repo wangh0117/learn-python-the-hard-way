341209ed1171ac3c252a7f2cbe50c39642f7bb56
wangh0117 <wangh0117@yeah.net>
2016-12-17 19:53:02 +0800

    ex12-14 files added

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv # unpacks: Take whatever is in argv, unpack it, and assign it to all of these variables on the left in order.
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
